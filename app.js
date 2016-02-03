98acdae24d9b8ddedf2a81df7f63d39dac42f669
ravbahra <ravbahra@gmail.com>
2016-02-03 16:29:57 +0000

    second commit

---
+++ app.js
@@ -0,0 +1,13 @@
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var socketio = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.send('<p>HELLLLLOOasdasdOOOOOOOOOOOO</p>');
+});
+
+
+http.listen(80, function () {
+    console.log('listening on *:3000');
+
+});
